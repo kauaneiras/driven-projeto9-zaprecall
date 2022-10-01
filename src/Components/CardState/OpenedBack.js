dd69345fdcc02537b7b18594a9418da62f2f3ebe
kauaneiras <kauante@hotmail.com>
2022-09-30 22:35:47 -0300

    [src/CardState/OpenedBack] --> Buttons layout <Nao Lembrei>, < Quase nao lembrei> and <zap> created

--- src/Components/CardState/OpenedBack.js
+++ src/Components/CardState/OpenedBack.js
@@ -3,16 +3,119 @@ import styled from "styled-components"
 export default function OpenedBack(props) {
     return (
         <Card>
-            <h1>{props.card.R}</h1>
+            <Answer><h1>{props.card.R}</h1></Answer>
+            <AlignButtons>
+                <NAOLEMBREI><h1>Não Lembrei</h1></NAOLEMBREI>
+                <QUASENAOLEMBREI><h1>Quase não lembrei</h1></QUASENAOLEMBREI>
+                <ZAP><h1>Zap!</h1></ZAP>
+            </AlignButtons>
+
         </Card>
     )
 }
 
 const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
     width: 300px;
     height: 131px;
     background-color: #FFFFD5;
     box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
     border-radius: 5px;
     margin-bottom: 25px;
+`
+
+const Answer = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: 90%;
+    height: 60%;
+    & h1{
+        font-size: 16px;
+        font-family: 'Recursive', sans-serif;
+        font-weight: 400;
+        line-height: 22px;
+
+`
+const AlignButtons = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 90%;
+    height: 40%;
+`
+
+const NAOLEMBREI = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 85.17px;
+    height: 37.17px;
+    background: #FF3030;
+    border-radius: 5px;
+    cursor: pointer;
+    border: 1px solid #FC1414;
+    & h1{
+        align-text: center;
+        font-family: 'Recursive', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 14px;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        color: #FFFFFF;
+    }
+`
+const QUASENAOLEMBREI = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 85.17px;
+    height: 37.17px;
+    background: #FF922E;
+    border-radius: 5px;
+    border: 1px solid #FC8823;
+    cursor: pointer;
+    & h1{
+        align-text: center;
+        font-family: 'Recursive', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 14px;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        color: #FFFFFF;
+    }
+`
+const ZAP = styled.button`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 85.17px;
+    height: 37.17px;
+    background: #2FBE34;
+    border-radius: 5px;
+    border: 1px solid #1BB720;
+    
+    cursor: pointer;
+    & h1{
+        align-text: center;
+        font-family: 'Recursive', sans-serif;
+        font-style: normal;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 14px;
+        display: flex;
+        align-items: center;
+        text-align: center;
+        color: #FFFFFF;
+    }
 `
--- src/Components/CardState/OpenedFront.js
+++ src/Components/CardState/OpenedFront.js
@@ -1,21 +1,59 @@
 import styled from "styled-components"
+import setinha from "../../assets/img/setinha.png"
 
 
 export default function OpenedFront(props) {
     return (
         <Card>
-            <h1>{props.card.Q}</h1>
-            <ion-icon name="reload-outline" onClick={(() => props.state("openedback"))}></ion-icon>
+            <Question><h1>{props.card.Q}</h1></Question>
+            
+            <RotateCard>
+                <img src={setinha} alt="setinha" onClick={() => props.state("openedback")}/>
+                {/* <ion-icon name="reload-outline" onClick={(() => props.state("openedback"))}></ion-icon> */}
+            </RotateCard>
         </Card>
     )
 }
 
 
 const Card = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
     width: 300px;
     height: 131px;
     background-color: #FFFFD5;
     box-shadow: 0px 4px 5px rgba(0, 0, 0, 0.15);
     border-radius: 5px;
     margin-bottom: 25px;
+`
+
+const Question = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    width: 90%;
+    height: 75%;
+    & h1{
+        font-size: 16px;
+        font-family: 'Recursive', sans-serif;
+        font-weight: 400;
+        line-height: 22px;
+
+`
+
+const RotateCard = styled.div`
+    height: 25%;
+    width: 90%;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    
+    & img {
+        width: 30px;
+        height: 20px;
+        cursor: pointer;
+    }
 `
